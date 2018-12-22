57fe8ce8da8ac77809aaf2f5af56e5ffefdce33a
melissaDerricottt <mderricott@higherachievement.org>
2018-12-21 20:33:23 -0500

    svg updates

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+import HeroCard from './components/HeroCard';
+import logo from './components/svgs/hydra.svg';
+
+
+// const styles = theme => ({
+//   root: {
+//     flexGrow: 1
+//   }
+// });
+
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+
+//       <img src={logo} alt="testing" />
+
+
+
+      <Grid container  spacing={16} >
+        <Grid item xs={12}>
+          <Grid container  justify="center" spacing={8}>
+            {[0, 1, 2,3,4].map(value => (
+              <Grid key={value} item>
+                <HeroCard  logo={logo}/>
+                
+              
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+      
+ 
+      
     );
   }
 }
 
 export default App;
+
+
---
+++ src/components/HeroCard.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+// import logo from './svgs/captainamerica.svg';
+
+
+const styles = {
+    card: {
+     width: 200,
+     height: 200,
+     borderRadius: 100
+    }
+}
+
+function HeroCard (props){
+    
+    return (
+        <Card style={styles.card}> 
+            
+        
+
+<img src={props.logo} alt="testing" />
+            
+        
+        
+        
+        </Card>
+    );
+}
+
+export default HeroCard
